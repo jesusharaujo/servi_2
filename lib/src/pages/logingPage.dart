b1ed8804f9d35e18136b9f8eb2a0a4260489cff0
jesusharaujo <jesusharaujo@gmail.com>
2019-11-24 21:02:57 -0700

    LOGIN FACEBOOK Y FIREBASE FUNCIONAL, AGREGUÉ FONT AWESOME

--- lib/src/pages/logingPage.dart
+++ lib/src/pages/logingPage.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as JSON;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key key}) : super(key: key);
@@ -14,7 +15,8 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
 
 final login = FacebookLogin();
-bool _isLoggedIn = false;
+final FirebaseAuth _auth = FirebaseAuth.instance;
+bool _isLoggedIn = false; 
 Map userProfile;
 
 void initiateFacebookLogin() async {
@@ -30,6 +32,17 @@ void initiateFacebookLogin() async {
       break;
     
     case FacebookLoginStatus.loggedIn:
+      FacebookAccessToken myToken = result.accessToken;
+
+      ///assuming sucess in FacebookLoginStatus.loggedIn
+      /// we use FacebookAuthProvider class to get a credential from accessToken
+      /// this will return an AuthCredential object that we will use to auth in firebase
+      AuthCredential credential= FacebookAuthProvider.getCredential(accessToken: myToken.token);
+
+      // this line do auth in firebase with your facebook credential.
+      FirebaseUser firebaseUser = (
+        await FirebaseAuth.instance.signInWithCredential(credential)
+      ).user;
       onLoginStatusChange();
       getUserInfo(result);
       break;
@@ -56,35 +69,41 @@ void getUserInfo(FacebookLoginResult result) async{
               'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email,picture&access_token=$token');
   final profile = JSON.jsonDecode(graphResponse.body);
   userProfile = profile;
-  print(profile);
 }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
+      // appBar: AppBar(
+      //   title: Text('Login Page'),
+      // ),
       body: _isLoggedIn 
-      ? Column(children: <Widget>[Text(userProfile['email']), FlatButton(child: Text('cerrar sesión'), onPressed: ()=>cerrarSesion(),)])
-      : Container(
-        child: Center(
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      ? Column(children: <Widget>[Text(userProfile['id']), FlatButton(child: Text('cerrar sesión'), onPressed: ()=>cerrarSesion(),)])
+      : Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('lib/src/assets/logo.png'),
+          SizedBox(height: 60.0),
+          RaisedButton(
+            elevation: 5.0,
+            splashColor: Colors.black,
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                RaisedButton(
-                  child: Text('Ingresar con Facebook'),
-                  color: Colors.blue,
-                  textColor: Colors.white,
-                  elevation: 7.0,
-                  onPressed: () {
-                    initiateFacebookLogin();
-                  },
-                )
+                Icon(FontAwesomeIcons.facebookSquare),
+                SizedBox(width: 10.0),
+                Text('Ingresar con Facebook', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),),
               ],
             ),
-        ),
-      )
+            color: Colors.blue,
+            textColor: Colors.white,
+            onPressed: () {
+              initiateFacebookLogin();
+            },
+          )
+        ],
+      ),
     );
   }
 
